9a910ffd9f5b74c1f9d39c732bd922182e7494a8
Gary J Davis <jonathan.davis1927@yahoo.com>
2026-06-23 23:53:17 -0500

    one step at a time

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { Header } from "./header/header";
+import { NavBar } from "./navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NavBar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'],
+  standalone: true
 })
 export class App {
   protected readonly title = signal('jd-suite-angular');
---
+++ src/app/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  // imports: [],
+  templateUrl: './header.html',
+  styleUrls: ['./header.scss'],
+})
+export class Header {
+
+}
